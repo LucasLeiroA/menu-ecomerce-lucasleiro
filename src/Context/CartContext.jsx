e9e9d9edd8a71713240d5fc7837608acae9068cd
Lucas Leiro <lucasleiroa@gmail.com>
2022-10-01 22:49:13 -0300

    se agrega el cart y se valida para poder borrar los elementos y que tampoco se puedan ingresar duplicados

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,35 +1,47 @@
-import React,{useState,createContext} from "react";
-
+import React, { useState, createContext } from "react";
 
 const cartContext = createContext();
 
+export default function CartContextProvider({ children }) {
+  const [cart, setCart] = useState([]);
 
-export default function CartContextProvider({children}){
-    
-    const [cart, setCart] = useState([]);
-    
-    function addItem(item,count){
-        let newCart = cart;
-        newCart.push({...item,count});
-        setCart(newCart);
+  function addItem(item, count) {
+    if (IsInCart(item.id)) {
+      let newCart = cart.map((itemMapeo) => {
+        if (itemMapeo.id === item.id) {
+          itemMapeo.count += count;
+          return itemMapeo;
+        } else {
+          return itemMapeo;
+        }
+      });
+      setCart(newCart);
+    } else {
+      let newCart = cart;
+      newCart.push({ ...item, count });
+      setCart(newCart);
     }
- 
-    function getTotalItemsInCart(){
-        let total = 0;
-        cart.forEach(item => total += item.count)
-        return total;
-    }
+  }
 
-    function clear(){
-        setCart([]);
-    }
-    
-    
-    return(
-        <cartContext.Provider value={{cart,addItem,getTotalItemsInCart,clear}}>
-            {children}
-        </cartContext.Provider>
-    )
+  function getTotalItemsInCart() {
+    let total = 0;
+    cart.forEach((item) => (total += item.count));
+    return total;
+  }
+  function IsInCart(id) {
+    let found = cart.some((item) => item.id === id);
+    return found;
+  }
+
+  function clear() {
+    setCart([]);
+  }
+
+  return (
+    <cartContext.Provider value={{ cart, addItem, getTotalItemsInCart, clear }}>
+      {children}
+    </cartContext.Provider>
+  );
 }
 
-export {cartContext};   
+export { cartContext };
